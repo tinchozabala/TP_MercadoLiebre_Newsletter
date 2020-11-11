a6a1a4d530d69c82be29fce5d5d4872d0933ece4
tinchozabala <martingaby2012@live.com>
2020-11-10 22:17:56 -0300

    tp terminado

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+const app = express();
+
+app.get ("/", function (req, res){
+    res.sendFile (__dirname + "/views/index.html")
+    })
+    
+    app.get ("*", function (req, res){
+        res.sendFile (__dirname + "/public" + req.url)
+    })
+    
+app.listen (3000, function(){
+    console.log ("para ver el sitio ingresa a http://localhost:3000")
+})
